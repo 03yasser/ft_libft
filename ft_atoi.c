962844f059063253fbdc43d6dc5ea1f204b61990
03yasser <yasserboutslighoi03@gmail.com>
2023-11-10 18:14:24 +0100

    .

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: yboutsli <yboutsli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 18:29:41 by yboutsli          #+#    #+#             */
-/*   Updated: 2023/11/07 21:34:20 by yboutsli         ###   ########.fr       */
+/*   Updated: 2023/11/10 18:12:33 by yboutsli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,28 @@
 
 int	ft_atoi(const char *str)
 {
-	int	signe;
-	int	i;
-	int	n;
+	int			sign;
+	int			i;
+	size_t		n;
+	long long	maxll;
 
+	maxll = 9223372036854775807;
 	i = 0;
-	signe = 1;
+	sign = 1;
 	n = 0;
 	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t' || str[i] == '\v'
 		|| str[i] == '\f' || str[i] == '\r')
 		i++;
 	if (str[i] == '-' || str[i] == '+')
 	{
-		if (str[i] == '-')
-			signe *= -1;
-		i++;
+		if (str[i++] == '-')
+			sign *= -1;
 	}
 	while (str[i] >= 48 && str[i] <= 57)
 	{
-		n = n * 10 + (str[i] - 48);
-		i++;
+		n = n * 10 + (str[i++] - 48);
+		if (n > maxll)
+			return ((sign > 0) ? -1 : 0);
 	}
-	return (n * signe);
+	return (n * sign);
 }
